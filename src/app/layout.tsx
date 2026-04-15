4bd6b630f20e2822f84018e0bdf57179f6246caf
Sufyan Farooq <127729780+Sufyan-Farooq@users.noreply.github.com>
2026-04-16 01:19:29 +0300

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { AppProvider } from '@/context/AppContext';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'NAQL (نقل) | Saudi Freight Aggregator',
@@ -36,6 +37,7 @@ export default function RootLayout({
       <body className="antialiased" suppressHydrationWarning>
         <AppProvider>
           {children}
+          <Analytics />
         </AppProvider>
       </body>
     </html>
